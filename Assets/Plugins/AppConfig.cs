05acf81735e98cf9a2ed063edaf0ae5a0d8fc0be
GANAZOR <79104637+GANAZOR@users.noreply.github.com>
2024-02-28 18:01:33 +0100

    Emotiv logic Base-game

---
+++ Assets/Plugins/AppConfig.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// Contain configuration of a specific App.
+/// </summary>
+static class AppConfig
+{
+    public static string AppUrl              = "wss://localhost:6868";
+    public static string AppName             = "UnityApp";
+    
+    // Please fill the clientId and client Secret of your application before starting.
+    public static string ClientId            = "oZn8biu0WgcpOxSFMhXEhzp5RYNrk1SaF6ju5zGa";
+    public static string ClientSecret        = "sd4KzynyjHe7G3eQQ5EmOcgbic2xNhYORG2pF6AYQXAQFkLqnrp4oPF30WrtM3gDRlkaaiYNPtIWNqtgz3D9uhPNXhj6LeuXh826gBJPhYFYz9j66bI6Rw2L4XXbsuV7";
+    public static string AppVersion          = "1.0.0";
+}
