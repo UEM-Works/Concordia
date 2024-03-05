ee89bb51bb9cbd7e28a0771478d5a1dbb376f126
GANAZOR <79104637+GANAZOR@users.noreply.github.com>
2024-03-05 11:04:15 +0100

    Assets and Shader for BreatheWorld + SkyBox + Door

---
+++ Assets/Scripts/roteSkyBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class roteSkyBox : MonoBehaviour
+{
+    //public Skybox cielo;
+    //float rotate;
+    //private void Start()
+    //{
+    //    rotate = Skybox
+    //}
+    //private void Update()
+    //{
+    //    rotate = rotate + 0.001f;
+    //}
+
+    public float RotateSpeed = 0.001f;
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+    }
+}
